b7616fecb3beda1ee06036a7193eb7640b8d532e
Cody Frazier <codyfrazier@me.com>
2019-12-18 09:48:51 -0800

    mostly working, need to format prices

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const API = 'https://acme-users-api-rev.herokuapp.com/api/';
-const productData = [];
-
-const loadData = async () => {
+async function loadData(){
+	const API = 'https://acme-users-api-rev.herokuapp.com/api/';
 	const responses = await Promise.all([
 		fetch(`${API}products`),
 		fetch(`${API}companies`),
@@ -12,29 +10,36 @@ const loadData = async () => {
 			return response.json();
 		})
 	);
+	
+	//***************************************************************
+	console.log(products);		//properly displayed.
+	console.log(companies);		//properly displayed.
+	console.log(offerings);		//properly displayed.
+	//***************************************************************
+	
+	renderProducts(products, companies, offerings);
+};
 
-	console.log(products);
-	console.log(companies);
-	console.log(offerings);
+function renderProducts(products, companies, offerings){
+	document.querySelector('#products').innerHTML = products.map(product => {
+		const offerList = offerings.filter(offer => {
+			return offer.productId === product.id;
+		}).reduce((acc, offer, idx) => {
+			coms = companies.filter(company => {
+				return company.id === offer.companyId;
+			});
+			acc += `<li>Offered by: ${ coms[0].name } at ${ offer.price }</li>`;
+			return acc;
+		}, ``);
+		return `
+			<div class='product-card'>
+			<h2 style='text-decoration:underline'>${ product.name }</h2>
+			<div>${ product.description }</div><br>
+			<div>$${ product.suggestedPrice }.00</div>
+			<ul>${ offerList }</ul>
+			</div>`;
+	}).join('');
+	
 };
 
 loadData();
-
-const displayList = document.querySelector('#products');
-
-const renderProducts = () => {
-	const html = productList
-		.map(
-			(entry) =>
-				`<div class='product-card'>
-      <div>${entry.name}</div>
-      <div>${entry.description}</div>
-      <div>${entry.suggestedPrice}</div>
-    </div>`
-		)
-		.join('');
-	displayList.innerHTML = html;
-};
-
-console.log(productData);
-renderProducts();
